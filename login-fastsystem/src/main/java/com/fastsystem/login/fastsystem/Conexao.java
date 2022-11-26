cb59d006edd6511436a98206c29f5ec072c4b992
Felipe Gomes <felipe.gomes@sptech.school>
2022-11-26 20:07:47 -0300

    Alterando a conexão com MySQL e MsAzure

--- login-fastsystem/src/main/java/com/fastsystem/login/fastsystem/Conexao.java
+++ login-fastsystem/src/main/java/com/fastsystem/login/fastsystem/Conexao.java
@@ -5,25 +5,34 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 public class Conexao {
     private JdbcTemplate connection;
+    private JdbcTemplate connectionAzure;
 
     public Conexao() {
         BasicDataSource dataSource = new BasicDataSource();
+        BasicDataSource dataSourceAzure = new BasicDataSource();
         
-        dataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        //Se estiver na faculdade mudar o localhost para "localhost:3306".
-        // dataSource.setUrl("jdbc:mysql://localhost:3306/FastSystem?useTimezone=true&serverTimezone=UTC");
+        //MySQL no Docker
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://172.17.0.2:3306/FastSystem?useTimezone=true&serverTimezone=UTC");
+        dataSource.setUsername("root");
+        dataSource.setPassword("urubu100");
+        
+        //SQL Server na Azure
+         dataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
         dataSource.setUrl("jdbc:sqlserver://fast-system-server.database.windows.net:1433;database=FastSystem;encryp t=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;");
-        //Se estiver na faculdade mudar o Username para "aluno" ou manter "root".
-        // dataSource.setUsername("aluno");
         dataSource.setUsername("admin-fast-system");  //221-1adsc-grupo10@bandtec.com.br
-        //Se estiver na faculdade mudar a senha para "sptech" ou "spt3ch".
-        // dataSource.setPassword("sptech");
         dataSource.setPassword("#Gfgrupo10");
-
+        
+        //Constructor
         this.connection = new JdbcTemplate(dataSource);
+        this.connection = new JdbcTemplate(dataSourceAzure);
     }
     
     public JdbcTemplate getConnection() {
         return connection;
+    }
+    
+    public JdbcTemplate getConnectionAzure() {
+        return connectionAzure;
     }
 }
