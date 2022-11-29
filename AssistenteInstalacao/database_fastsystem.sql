642fb77080178ddcc5754c3c8686691c0ff184a1
Rafael-Caxixi <rafael.fuzeti@sptech.school>
2022-11-28 21:22:57 -0300

    Script no assistente alterado

--- AssistenteInstalacao/database_fastsystem.sql
+++ AssistenteInstalacao/database_fastsystem.sql
@@ -1,26 +1,28 @@
 CREATE DATABASE FastSystem;
 USE FastSystem;
 
+-- DROP DATABASE FastSystem;
+
 CREATE TABLE Empresa(
 id_empresa INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 nome_empresa VARCHAR(100),
 cnpj_empresa VARCHAR (14),
 cep_empresa VARCHAR(11),
 numero_empresa INT,
-telefone_empresa VARCHAR(13),
+telefone_empresa VARCHAR(14),
 representante VARCHAR(100),
 email_empresa VARCHAR(50)
 )AUTO_INCREMENT = 0;
 
-CREATE TABLE Funcionario(
+CREATE TABLE Funcionario (
 id_funcionario INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 fk_empresa INT,
 nome_funcionario VARCHAR(100),
-is_admin BINARY(1),
+is_admin BOOLEAN,
 cpf_funcionario VARCHAR(11),
 email_funcionario VARCHAR(50),
 senha_funcionario VARCHAR(25),
-telefone_funcionario VARCHAR(13),
+telefone_funcionario VARCHAR(14),
 FOREIGN KEY(fk_empresa) REFERENCES Empresa(id_empresa)
 )AUTO_INCREMENT = 100;
 
@@ -54,7 +56,7 @@ CREATE TABLE Registro_Processo(
 id_registro_processo INT PRIMARY KEY AUTO_INCREMENT,
 nome_processo VARCHAR(45),
 data_hora DATETIME,
-is_autorizado BINARY(1),
+is_autorizado BOOLEAN,
 fk_maquina INT,
 FOREIGN KEY (fk_maquina) references Maquina(id_maquina)
 );
@@ -62,7 +64,7 @@ FOREIGN KEY (fk_maquina) references Maquina(id_maquina)
 CREATE TABLE Componente(
 id_componente INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 nome_componente VARCHAR(45),
-is_ativo BINARY(1),
+is_ativo BOOLEAN,
 fabricante_componente VARCHAR(45),
 modelo_componente VARCHAR(100),
 capacidade_componente FLOAT,
@@ -85,45 +87,32 @@ FOREIGN KEY(fk_tipo_registro) REFERENCES Tipo_Registro(id_tipo_registro)
 );
 
 INSERT INTO Empresa VALUES 
-(null, 'FastSystem', 123456789, 02535412, 1522, 11942563656, 'Endryl', "endryl@gmail.com"),
-(null, 'McDonalds', 987654321, 32654845, 365, 11953145796, 'Donald McDonalds', 'dodo@gmail.com');
-
-INSERT INTO Maquina VALUES 
-( null, 1, "DESKTOP", "Desktop 1", '', 0, 'felipe.fastsystem@gmail.com', '1234' ),  
-( null, 2, "TOTEM", "Totem 1", '', 0, 'endryl.mcdonalds@gmail.com', '12345'),
-( null, 2, "DESKTOP", "Desktop 1", '', 0, 'vitoria.mcdonalds@gmail.com', '12346');
-
-INSERT INTO Tipo_Registro VALUES
-( null, 'GB' ),
-( null, '%' );
+(null, 'Fast Food 1', 123456789, 02535412, 1522, 11942563656, 'Endryl', "admin.fastfood1@gmail.com"),
+(null, 'Fast Food 2', 987654321, 32654845, 365, 11953145796, 'Donald McDonalds', 'admin.fastfood2@gmail.com');
 
 INSERT INTO Funcionario VALUES 
 (null, 1, 'Endryl', 1, 12345678912, 'endryl@gmail.com', 12345678, '942518747'),
-(null, 2, 'Felipe', 1, 98765432112, 'felipe@gmail.com', 12345678, '965321547');
+(null, 1, 'Alex', 0, 12345678912, 'alex@gmail.com', 12345678, '942518747'),
+(null, 2, 'Felipe', 1, 98765432112, 'felipe@gmail.com', 12345678, '965321547'),
+(null, 2, 'Gerson', 0, '12345678912', 'gerson@gmail.com', 12345678, '942518747');
+
+INSERT INTO Maquina VALUES 
+(null, 1, 'DESKTOP', 'Admin PC', '', 0, 'admin.fastfood1@gmail.com', '1234' ),
+(null, 2, 'TOTEM', 'Caixa 1', '', 0, 'caixa1.fastfood2@gmail.com', '1234'),
+(null, 2, 'DESKTOP', 'Admin PC', '', 0, 'admin.fastfood2@gmail.com', '1234'),
+(null, 2, 'DESKTOP', 'Caixa 2', '', 0, 'caixa2.fastfood2@gmail.com', '1234' ),
+(null, 2, 'TOTEM', 'Caixa 3', '', 0, 'caixa3.fastfood2@gmail.com', '1234'),
+(null, 2, 'DESKTOP', 'Totem 1', '', 0, 'totem1.fastfood2s@gmail.com', '1234'),
+(null, 2, 'TOTEM', 'Totem 2', '', 0, 'totem2.fastfood2@gmail.com', '1234'),
+(null, 2, 'DESKTOP', 'Totem 3', '', 0, 'totem3.fastfood2@gmail.com', '1234'), 
+(null, 2, 'DESKTOP', 'Caixa 4', '', 0, 'caixa4.fastfood2@gmail.com', '1234' ); 
 
 INSERT INTO App VALUES
-( null, 'chrome' ),
-( null, 'WhatsApp' ),
-( null, 'AnyDesk' );
+(null, 'chrome'),
+(null, 'WhatsApp'),
+(null, 'AnyDesk'),
+(null, 'Code');
 
-
-        
-UPDATE Componente SET 
-	nome_componente = 'Processadorrrrr', 
-	is_ativo = true, 
-    fabricante_componente = 'GenuineIntel', 
-    modelo_componente = 'Intel(R) Core(TM) i7-10610U CPU @ 1.80GHz', 
-    capacidade_componente = 10,
-    fk_maquina = 1
-		WHERE id_componente = 1 AND nome_componente LIKE 'Processador%';
-        
-UPDATE Componente SET 
-	nome_componente = 'Memóriaaaa', 
-	is_ativo = true,
-    capacidade_componente = 10,
-    fk_maquina = 1
-		WHERE id_componente = 2 AND nome_componente LIKE 'Memória%';
-        
-UPDATE Componente SET 
-	nome_componente = 'Disco 1 BEBE', is_ativo = true, modelo_componente = 'SSSTC CA5-8D256-Q79 (Standard disk drives)', capacidade_componente = 256
-    WHERE id_componente = 3 AND nome_componente LIKE 'Disco 1%';
+INSERT INTO Tipo_Registro VALUES
+(null, 'GB'),
+(null, '%');
